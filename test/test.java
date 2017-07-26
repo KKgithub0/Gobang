5f4675690c830b4cf7dc237c05188796c4b2d8d5
Snowman <xukai_bx@163.com>
2017-07-26 08:02:26 +0800

    Add files via upload

---
+++ test/test.java
@@ -0,0 +1,18 @@
+package org.liky.game.test;
+
+
+import org.liky.game.frame.FiveChessFrame;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FiveChessFrame ff = new FiveChessFrame();
+	
+		
+		}
+	
+
+}
